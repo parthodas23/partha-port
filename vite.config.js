3db3f6cbc78e855d57cdf39f456c7dd9e096d756
Partha Das <parthodasm@gmail.com>
2025-11-27 20:19:26 +0600

    Pushed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/partha-portfolio/",
+  base: "/partha-port/",
   server: {
     host: true, // or use '0.0.0.0'
     port: 5173,
